24b60b786b3e0ab0430c3c8b1d1c830228bb5850
ArhumIshtiaq <arhumishi@gmail.com>
2019-11-17 12:58:39 +0500

    added model/modelforms

--- Project Prototype/Django/pracsite/polls/forms.py
+++ Project Prototype/Django/pracsite/polls/forms.py
@@ -1,77 +1,97 @@
 from django import forms
+from polls.models import *
+
 
 class AddForm(forms.Form):
-    #fields for interpreters, clients, content, students
-    CATEGORY_CHOICES= [
-    ('interpreters', 'Interpreters'),
-    ('ind_cust', 'Individual Customer'),
-    ('company', 'Company'),
-    ('content', 'Content'),
-    ('students', 'Students'),
+    # fields for interpreters, clients, content, students
+    CATEGORY_CHOICES = [
+        ('interpreters', 'Interpreters'),
+        ('ind_cust', 'Individual Customer'),
+        ('company', 'Company'),
+        ('content', 'Content'),
+        ('students', 'Students'),
     ]
-    mode = forms.CharField(label='Category', 
-    widget=forms.Select(choices=CATEGORY_CHOICES))
+    mode = forms.CharField(label='Category',
+                           widget=forms.Select(choices=CATEGORY_CHOICES))
 
-class InterpreterForm(forms.Form):
+
+class InterpreterForm(forms.ModelForm):
     GENDER_CHOICES = [
         ('male', 'Male'),
         ('female', 'Female'),
     ]
 
     name = forms.CharField(label='Name')
-    age = forms.CharField(label='Age')
+    age = forms.IntegerField(label='Age')
     gender = forms.CharField(label='Gender',
-    widget=forms.Select(choices=GENDER_CHOICES))
+                             widget=forms.Select(choices=GENDER_CHOICES))
     mobile_no = forms.CharField(label='Mobile Number')
     nic_no = forms.CharField(label='CNIC Number')
     address = forms.CharField(label='Address')
 
-class CustomerForm(forms.Form):
+    class Meta:
+        model = interpreter
+        fields = ('name', 'age', 'nic_no', 'address', 'mobile_no',
+                  'gender', 'calls_served', 'average_rating', 'date_of_joining')
+
+
+class CustomerForm(forms.ModelForm):
     GENDER_CHOICES = [
         ('male', 'Male'),
         ('female', 'Female'),
     ]
 
     name = forms.CharField(label='Name')
-    age = forms.CharField(label='Age')
+    age = forms.IntegerField(label='Age')
     gender = forms.CharField(label='Gender',
-    widget=forms.Select(choices=GENDER_CHOICES))
+                             widget=forms.Select(choices=GENDER_CHOICES))
     mobile_no = forms.CharField(label='Mobile Number')
     nic_no = forms.CharField(label='CNIC Number')
     address = forms.CharField(label='Address')
 
-class CompanyForm(forms.Form):
+    class Meta:
+        model = customer
+        fields = ('name', 'age', 'nic_no', 'address', 'mobile_no',
+                  'gender', 'date_of_joining')
+
+
+class CompanyForm(forms.ModelForm):
     name = forms.CharField(label='Name')
     address = forms.CharField(label='Address')
     poc_name = forms.CharField(label='PoC Name')
     poc_mobile_no = forms.CharField(label='PoC Mobile Number')
 
-class ContentForm(forms.Form):
+    class Meta:
+        model = company
+        fields = ('name', 'address', 'poc_name',
+                  'poc_mobile_no', 'date_of_joining')
+
+
+class ContentForm(forms.ModelForm):
     title = forms.CharField(label='Title')
     link = forms.CharField(label='Link')
     date_of_release = forms.DateField(label='Date of Release')
     
 
-class StudentsForm(forms.Form):
+class StudentsForm(forms.ModelForm):
     name = forms.CharField(label='Name')
-    age = forms.CharField(label='Age')
+    age = forms.IntegerField(label='Age')
     mobile_no = forms.CharField(label='Mobile Number')
     level = forms.CharField(label='Level')
     classification = forms.CharField(label='Classification')
     occupation = forms.CharField(label='Occupation')
     city = forms.CharField(label='City')
-    batch_no = forms.CharField(label='Batch Number')
+    batch_no = forms.IntegerField(label='Batch Number')
 
-class SearchForm(forms.Form):
+
+class SearchForm(forms.ModelForm):
     name = forms.CharField(label='Name/Title')
-    CATEGORY_CHOICES= [
-    ('interpreters', 'Interpreters'),
-    ('ind_cust', 'Individual Customer'),
-    ('company', 'Company'),
-    ('content', 'Content'),
-    ('students', 'Students'),
+    CATEGORY_CHOICES = [
+        ('interpreters', 'Interpreters'),
+        ('ind_cust', 'Individual Customer'),
+        ('company', 'Company'),
+        ('content', 'Content'),
+        ('students', 'Students'),
     ]
-    mode = forms.CharField(label='Category', 
-    widget=forms.Select(choices=CATEGORY_CHOICES))
-    
-    
+    mode = forms.CharField(label='Category',
+                           widget=forms.Select(choices=CATEGORY_CHOICES))
--- Project Prototype/Django/pracsite/polls/models.py
+++ Project Prototype/Django/pracsite/polls/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+
+class interpreter(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    mobile_no = models.IntegerField()
+    nic_no = models.IntegerField()
+    address = models.CharField(max_length=200)
+    calls_served = models.IntegerField()
+    average_rating = models.FloatField()
+    date_of_joining = models.DateField()
+
+class customer(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    mobile_no = models.IntegerField()
+    nic_no = models.IntegerField()
+    address = models.CharField(max_length=200)
+    date_of_joining = models.DateField()
+
+class company(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    poc_name = models.CharField(max_length=50)
+    poc_mobile_no = models.IntegerField()
+    date_of_joining = models.DateField()
